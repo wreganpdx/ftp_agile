0ec903fb721b313e584f5f1e10694e2ac973dc1f
Berin-Hadziabdic <45540078+Berin-Hadziabdic@users.noreply.github.com>
2019-07-05 10:19:04 -0700

    Added user credential functionality.

--- FTPServer/Program.cs
+++ FTPServer/Program.cs
@@ -13,14 +13,26 @@ namespace FTPServer
         public static void LogIn()
         {
             string ip; // ip will be a string type
+            string username;
+            string password;
             FtpClient client;
 
             
             Console.WriteLine("DEBUG MSG: Localhost IP is 127.0.0.1");
-            Console.WriteLine("Enter the I.P to connect to and press enter: ");
+            Console.Write("Enter the I.P to connect to and press enter: ");
             ip = Console.ReadLine(); //read in ip
 
+            Console.WriteLine(""); //spacing for console
+            Console.WriteLine("Leaving username and password field empty attempts to connect with anonymous account.");
+            Console.WriteLine(""); //spacing for console
+
+            Console.Write("Enter the username field to connect with and press enter: ");
+            username = Console.ReadLine(); //read in username
+            Console.Write("Enter the password field to connect with and press enter: ");
+            password = Console.ReadLine(); //read in password
+
             client = new FtpClient(ip); // create an FTP client using ip
+            client.Credentials = new System.Net.NetworkCredential(username, password); //Create credentials
             client.Connect(); //Connect to client
 
             if(client.IsConnected) //If connect success
