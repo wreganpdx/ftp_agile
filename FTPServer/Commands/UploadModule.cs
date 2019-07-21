503faa264b4ee2dabde681f27e8cd56aebfddddf
Berin-Hadziabdic <45540078+Berin-Hadziabdic@users.noreply.github.com>
2019-07-21 15:23:40 -0700

    Update UploadModule.cs

--- FTPServer/Commands/UploadModule.cs
+++ FTPServer/Commands/UploadModule.cs
@@ -141,7 +141,13 @@ namespace FTPServer.Commands
                 newDirectoryRemote = Console.ReadLine();
                 Console.WriteLine("");
 
-            if(FTPServer.Commands.ResourcePathCheck.checkPathCharacters(newDirectoryRemote))
+            if(client.DirectoryExists(newDirectoryRemote))
+            {
+                Console.WriteLine("The directory already exist.");
+                Console.WriteLine("Press t to try again or any other char to quit: ");
+                cont = Console.ReadKey().KeyChar;
+            }
+            else if(FTPServer.Commands.ResourcePathCheck.checkPathCharacters(newDirectoryRemote))
             {
                 Console.WriteLine("Creating directory with the name " + newDirectoryRemote);
                 Console.WriteLine("___________________________________________");
