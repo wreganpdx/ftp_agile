a00f13c4fb5f2850400320ba2f49fad0afd8f560
Berin-Hadziabdic <45540078+Berin-Hadziabdic@users.noreply.github.com>
2019-07-25 15:28:26 -0700

    Updated Program.cs

---
+++ FTPServer/Program.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentFTP;
+using FTPServer.Commands;
+using System.IO;
+
+
+namespace FTPServer
+{
+
+    class Program
+    {
+        static FtpClient client;
+
+        public static void OptionPrompt()
+        {
+            string newAction = null;
+
+            while(newAction != "0")
+            {
+                Console.WriteLine("");
+                Console.Write("Please enter a command\n");
+                Console.Write("0. Exit application.\n");
+                Console.Write("1. Get file from remote server\n");
+                Console.Write("2. Log off from remote server\n");
+                Console.Write("3. Get multiple files\n");
+                Console.Write("4. List directories and files on local machine\n");
+                Console.Write("5. Delete directory on remote server\n");
+                Console.Write("6. Create directory on remote server\n");
+                Console.Write("7. Put file on remote server\n");
+                Console.Write("8. Rename a local file.\n");
+
+
+
+                newAction = Console.ReadLine(); //read in ip
+                int action = Convert.ToInt32(newAction);
+
+                switch (action)
+                {
+                    case 0:
+                        Console.WriteLine("Bye!");
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        Commands.DeleteDir.deleteDir(client);
+                        break;
+                    case 6:
+                        Commands.MakeDir.makeDir(client);
+                        break;
+                    case 7:
+                        Commands.PutFile.putFile(client);
+                        break;
+                    case 8:
+                        Commands.RenameFile.renameFile(client);
+                        break;
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+           //192.168.1.11
+           Console.WriteLine("FTP CLIENT" + Environment.NewLine);
+
+           client = Commands.LogIn.logIn();
+           
+          if(client != null && client.IsConnected)
+             Program.OptionPrompt();
+        }
+    }
+}
