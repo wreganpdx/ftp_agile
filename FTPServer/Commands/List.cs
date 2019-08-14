6bb0ce6b6980c6c2e9a24432f536eeb52e673b45
Robert Pelayo <rpelayo@pdx.edu>
2019-08-13 19:43:56 -0700

    Added local ls and log history

---
+++ FTPServer/Commands/List.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace FTPServer.Commands
+{
+    class List
+    {
+        public static void DirSearch()
+        {
+            try
+            {
+                string working_directory;
+                Console.WriteLine("What is the absolute path of the directory you want displayed locally?");
+                working_directory = Console.ReadLine();
+                foreach (string d in Directory.GetDirectories(working_directory))
+                {
+                    Console.WriteLine(d);
+                }
+                foreach (string f in Directory.GetFiles(working_directory))
+                {
+                    Console.WriteLine(f);
+                }
+
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
